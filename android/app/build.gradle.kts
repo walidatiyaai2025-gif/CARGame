7d362222e1608f87151d93d24966865a82a46bf7
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-04 19:51:24 +0300

    Migrate Kotlin jvmTarget to compilerOptions DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
 android {
     namespace = "com.walka.cargosort"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     defaultConfig {
         applicationId = "com.walka.cargosort"
         minSdk = 23
@@ -16,6 +29,14 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-    buildTypes { release { signingConfig = signingConfigs.getByName("debug") } }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
-flutter { source = "../.." }
+
+flutter {
+    source = "../.."
+}
