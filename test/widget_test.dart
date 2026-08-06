38dd83c25aa52064441d5f145ba28c7bf17db366
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-06 13:01:39 +0300

    Fix widget test for required app settings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:cargo_sort_game/core/settings/app_settings_store.dart';
 import 'package:cargo_sort_game/core/storage/progress_store.dart';
 import 'package:cargo_sort_game/main.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -5,8 +6,16 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('home screen starts', (tester) async {
     final store = ProgressStore();
-    await tester.pumpWidget(CargoSortApp(store: store));
+    final settings = AppSettingsStore();
+
+    await tester.pumpWidget(
+      CargoSortApp(
+        store: store,
+        settings: settings,
+      ),
+    );
     await tester.pumpAndSettle();
+
     expect(find.text('Cargo Sort'), findsOneWidget);
   });
 }
