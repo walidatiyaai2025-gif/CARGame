2468b5c250688aed6680df4e10638ab48632efda
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-06 12:28:12 +0300

    Add persistent game settings store

---
+++ lib/core/settings/app_settings_store.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettingsStore extends ChangeNotifier {
+  static const _soundKey = 'settings_sound';
+  static const _musicKey = 'settings_music';
+  static const _vibrationKey = 'settings_vibration';
+
+  final SharedPreferencesAsync _prefs = SharedPreferencesAsync();
+
+  bool soundEnabled = true;
+  bool musicEnabled = true;
+  bool vibrationEnabled = true;
+
+  Future<void> load() async {
+    soundEnabled = await _prefs.getBool(_soundKey) ?? true;
+    musicEnabled = await _prefs.getBool(_musicKey) ?? true;
+    vibrationEnabled = await _prefs.getBool(_vibrationKey) ?? true;
+    notifyListeners();
+  }
+
+  Future<void> setSound(bool value) async {
+    soundEnabled = value;
+    notifyListeners();
+    await _prefs.setBool(_soundKey, value);
+  }
+
+  Future<void> setMusic(bool value) async {
+    musicEnabled = value;
+    notifyListeners();
+    await _prefs.setBool(_musicKey, value);
+  }
+
+  Future<void> setVibration(bool value) async {
+    vibrationEnabled = value;
+    notifyListeners();
+    await _prefs.setBool(_vibrationKey, value);
+  }
+}
