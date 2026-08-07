5cc555aafd8765797b22a4330b20d03b35ac2483
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 05:33:45 +0300

    test(motion): initialize world map preferences

--- test/features/levels/level_select_ambient_motion_test.dart
+++ test/features/levels/level_select_ambient_motion_test.dart
@@ -3,8 +3,17 @@ import 'package:cargo_sort_game/core/storage/progress_store.dart';
 import 'package:cargo_sort_game/features/levels/level_select_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
+
   testWidgets('world map reuses the reduced-motion ambient scene', (
     tester,
   ) async {
