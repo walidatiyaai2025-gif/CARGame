ef8e40da269539a1d6979dc9c515d56a609f1373
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 20:48:38 +0300

    Test shop booster GamePanel integration

---
+++ test/features/shop/shop_game_panel_test.dart
@@ -0,0 +1,21 @@
+import 'package:cargo_sort_game/core/storage/progress_store.dart';
+import 'package:cargo_sort_game/core/widgets/game_panel.dart';
+import 'package:cargo_sort_game/features/shop/shop_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('shop booster offers use the shared GamePanel shell', (tester) async {
+    final store = ProgressStore();
+
+    await tester.pumpWidget(
+      MaterialApp(home: ShopScreen(store: store)),
+    );
+    await tester.pump();
+
+    expect(find.text('Smart Hint Pack'), findsOneWidget);
+    expect(find.text('Extra Moves Pack'), findsOneWidget);
+    expect(find.text('Combo Shield'), findsOneWidget);
+    expect(find.byType(GamePanel), findsNWidgets(3));
+  });
+}
