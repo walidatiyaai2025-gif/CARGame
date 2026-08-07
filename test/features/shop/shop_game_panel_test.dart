a25dceb9095652ee05f481a255f7a7fe944d00ef
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 21:27:26 +0300

    test: initialize shop panel preferences fixture

--- test/features/shop/shop_game_panel_test.dart
+++ test/features/shop/shop_game_panel_test.dart
@@ -3,8 +3,17 @@ import 'package:cargo_sort_game/core/widgets/game_panel.dart';
 import 'package:cargo_sort_game/features/shop/shop_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
+
   testWidgets('shop booster offers use the shared GamePanel shell', (tester) async {
     final store = ProgressStore();
 
