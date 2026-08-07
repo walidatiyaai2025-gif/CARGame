058d6b57f6c8f4af3fc9ee207e16d587d1790048
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 21:27:06 +0300

    test: initialize progress panel preferences fixture

--- test/features/progress/progress_hub_game_panel_test.dart
+++ test/features/progress/progress_hub_game_panel_test.dart
@@ -3,9 +3,16 @@ import 'package:cargo_sort_game/core/widgets/game_panel.dart';
 import 'package:cargo_sort_game/features/progress/progress_hub_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
 
   testWidgets('progress hub adopts GamePanel while preserving long-form scrolling', (
     tester,
