430b8d8e17340ce2e0ef431e5e7732ef6a4c35c0
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 05:37:23 +0300

    test(game): initialize motion test preferences

--- test/features/game/game_screen_motion_test.dart
+++ test/features/game/game_screen_motion_test.dart
@@ -5,8 +5,17 @@ import 'package:cargo_sort_game/features/game/game_screen.dart';
 import 'package:cargo_sort_game/features/game/level_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
+
   testWidgets('gameplay ignores warehouse input while cargo is resolving', (
     tester,
   ) async {
