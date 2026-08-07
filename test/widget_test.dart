58891da18d2f2d553991b35fdbe40ff7baa61690
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 03:02:00 +0300

    test: initialize async preferences for home smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,17 @@ import 'package:cargo_sort_game/core/settings/app_settings_store.dart';
 import 'package:cargo_sort_game/core/storage/progress_store.dart';
 import 'package:cargo_sort_game/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
+
   testWidgets('home screen starts', (tester) async {
     final store = ProgressStore();
     final settings = AppSettingsStore();
