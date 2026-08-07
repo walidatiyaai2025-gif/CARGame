7f11046f9d4a508d95077a5c55903050282d5f95
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 06:24:14 +0300

    feat(ads): add banner test ad unit

--- lib/core/ads/ad_service.dart
+++ lib/core/ads/ad_service.dart
@@ -4,6 +4,10 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdService {
   // Official Google test IDs. Never use real IDs during development.
+  static String get bannerId => Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/6300978111'
+      : 'ca-app-pub-3940256099942544/2934735716';
+
   static String get rewardedId => Platform.isAndroid
       ? 'ca-app-pub-3940256099942544/5224354917'
       : 'ca-app-pub-3940256099942544/1712485313';
