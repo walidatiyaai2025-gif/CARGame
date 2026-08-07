0aec2d62647fdc1ffd3f671b3f10810590b2d1d0
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 21:04:25 +0300

    test(ci): initialize shared preferences in panel tests

--- test/features/home/home_shared_panels_test.dart
+++ test/features/home/home_shared_panels_test.dart
@@ -7,9 +7,16 @@ import 'package:cargo_sort_game/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
 
   testWidgets(
     'home uses shared resource/action panels without adding scrolling',
