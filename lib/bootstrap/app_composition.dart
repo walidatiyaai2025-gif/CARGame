c2f041ee91259f1ee4fe764d72e1aab2c90d469c
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-10 02:00:35 +0300

    ENG-012: compose disabled analytics boundary

--- lib/bootstrap/app_composition.dart
+++ lib/bootstrap/app_composition.dart
@@ -1,5 +1,8 @@
 import '../core/ads/ad_consent_controller.dart';
+import '../core/analytics/privacy_gated_analytics.dart';
+import '../core/application/analytics_port.dart';
 import '../core/application/optional_service_port.dart';
+import '../core/config/app_build_config.dart';
 import '../core/services/optional_service_coordinator.dart';
 import '../core/settings/app_settings_store.dart';
 import '../core/storage/progress_store.dart';
@@ -9,8 +12,10 @@ class AppComposition {
     required this.progressStore,
     required this.settingsStore,
     required this.optionalServices,
+    AnalyticsPort? analytics,
     AdConsentController? adConsent,
-  }) : adConsent = adConsent ?? AdConsentController.production();
+  }) : analytics = analytics ?? PrivacyGatedAnalytics.disabled(),
+       adConsent = adConsent ?? AdConsentController.production();
 
   factory AppComposition.production() => AppComposition(
     progressStore: ProgressStore(),
@@ -19,12 +24,17 @@ class AppComposition {
       defaultTimeout: const Duration(seconds: 20),
       maxAttempts: 3,
     ),
+    analytics: PrivacyGatedAnalytics(
+      configEnabled: AppBuildConfig.current.enableAnalytics,
+      privacy: const DenyAllAnalyticsPrivacy(),
+    ),
     adConsent: AdConsentController.production(),
   );
 
   final ProgressStore progressStore;
   final AppSettingsStore settingsStore;
   final OptionalServicePort optionalServices;
+  final AnalyticsPort analytics;
   final AdConsentController adConsent;
 
   Future<void> dispose() => optionalServices.dispose();
