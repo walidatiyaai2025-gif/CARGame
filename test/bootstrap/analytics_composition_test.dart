4f48556036bb9dce484ad540ab37f02a0dbacb27
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-10 02:01:20 +0300

    ENG-012: verify production analytics fail-closed

---
+++ test/bootstrap/analytics_composition_test.dart
@@ -0,0 +1,14 @@
+import 'package:cargo_sort_game/bootstrap/app_composition.dart';
+import 'package:cargo_sort_game/core/application/analytics_port.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('production composition exposes analytics but keeps collection off', () async {
+    final composition = AppComposition.production();
+
+    expect(composition.analytics, isA<AnalyticsPort>());
+    expect(composition.analytics.isCollectionEnabled, isFalse);
+
+    await composition.dispose();
+  });
+}
