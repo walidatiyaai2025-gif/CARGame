d710a6af0c5e29a9c74e5a0b852d4dbf45a52e64
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-12 18:39:34 +0300

    RT3D-002: register native Filament platform view

--- android/app/src/main/kotlin/com/walka/cargosort/MainActivity.kt
+++ android/app/src/main/kotlin/com/walka/cargosort/MainActivity.kt
@@ -1,3 +1,14 @@
 package com.walka.cargosort
+
 import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.platformViewsController.registry.registerViewFactory(
+            NativeFilamentSceneView.VIEW_TYPE,
+            NativeFilamentSceneFactory(flutterEngine.dartExecutor.binaryMessenger),
+        )
+    }
+}
