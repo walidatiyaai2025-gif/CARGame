9e435ce259d5663845da1f92a551fd406cc82f98
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-18 18:17:38 +0300

    style: apply canonical Dart format

--- test/features/game/game_screen_motion_test.dart
+++ test/features/game/game_screen_motion_test.dart
@@ -127,9 +127,7 @@ void main() {
       );
       await tester.pump();
 
-      await tester.tap(
-        find.byKey(ValueKey('house-1-cargo-${selected.id}-0')),
-      );
+      await tester.tap(find.byKey(ValueKey('house-1-cargo-${selected.id}-0')));
       await tester.pump();
 
       final selectedWarehouse = find.byKey(
